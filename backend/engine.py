3c9873e0c0ab0a6ca083f4595cbc854e91abba3d
Synchromanical <alexcen35@gmail.com>
2023-08-03 21:12:20 -0400

    Part 3

--- backend/engine.py
+++ backend/engine.py
@@ -1,11 +1,23 @@
 import pickle 
 from sklearn.metrics.pairwise import cosine_similarity
+import requests
+import config
 
 df1 = pickle.load(open('./data/movie_list.pkl', 'rb'))
 tfidf_matrix = pickle.load(open('./data/tfidf_matrix.pkl', 'rb'))
 
 def get_recommendation(title):
-    idx = df1[df1['title'] == title].index[0]
+    #idx = df1[df1['title'] == title].index[0]
+
+    try:
+        idx = df1[df1['title'] == title].index[0]
+    except:
+        try:
+            result = requests.get("https://api.themoviedb.org/3/search/movie?api_key="+config.api_key+"&query="+title)
+            title = result.json()['results'][0]['title']
+            idx = df1[df1['title'] == title].index[0]
+        except:
+            return "No Movies Found"
     
     sim_score = list(enumerate(cosine_similarity(tfidf_matrix, tfidf_matrix[idx])))
     
@@ -30,6 +42,6 @@ def get_recommendation(title):
     
     return recommendation
 
-print(get_recommendation('The Dark Knight'))
+#print(get_recommendation('The Dark Knight'))
     
     
